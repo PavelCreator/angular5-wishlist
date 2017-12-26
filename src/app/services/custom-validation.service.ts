35f7c323abf419cb8d6941213c2f19962dfe3681
pavelcreator <pavelcreator@gmail.com>
2017-12-26 02:49:32 +0200

    custom email validation new RegExp

--- src/app/services/custom-validation.service.ts
+++ src/app/services/custom-validation.service.ts
@@ -12,7 +12,7 @@ export class CustomValidationService {
   }
 
   email(control: FormGroup) {
-    const EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    const EMAIL_REGEXP: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (control.value !== '' && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
       return {'email': true};
     }
